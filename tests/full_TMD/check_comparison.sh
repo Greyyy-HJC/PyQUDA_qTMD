bf3ec33b7e8b8bfeaa8d68543dc76c4e7f29e28e
Jinchen He <jinchen@aurora-uan-0011.hostmgmt.cm.aurora.alcf.anl.gov>
2025-12-16 23:39:23 +0000

    finished comparison between local and Aurora, one GPU different backend

---
+++ tests/full_TMD/check_comparison.sh
@@ -0,0 +1,8 @@
+#* TEST: run the same code with 1 GPU on local machine (NVIDIA & cupy) and Aurora (Intel & dpnp)
+#* The data is generated from check_local.py and check_aurora.py
+
+python compare_h5.py /lus/flare/projects/StructNGB/jinchen/package/PyQUDA_qTMD/tests/full_TMD/data/c2pt/S8T8_aurora.c2pt.0.ex.x7y3z5t7.1HYP_GSRC_W90_k3_T5.h5 /lus/flare/projects/StructNGB/jinchen/package/PyQUDA_qTMD/tests/full_TMD/data/c2pt/S8T8_local.c2pt.0.ex.x7y3z5t7.1HYP_GSRC_W90_k3_T5.h5 > logs/diff_2pt.txt
+
+python compare_h5.py /lus/flare/projects/StructNGB/jinchen/package/PyQUDA_qTMD/tests/full_TMD/data/qTMD/S8T8_aurora.qTMD.0.CG.D.ex.x7y3z5t7.1HYP_GSRC_W90_k3_T5.PX0PY0PZ9dt4.PpUnpol.5.h5 /lus/flare/projects/StructNGB/jinchen/package/PyQUDA_qTMD/tests/full_TMD/data/qTMD/S8T8_local.qTMD.0.CG.D.ex.x7y3z5t7.1HYP_GSRC_W90_k3_T5.PX0PY0PZ9dt4.PpUnpol.5.h5 > logs/diff_3pt_CG.txt
+
+python compare_h5.py /lus/flare/projects/StructNGB/jinchen/package/PyQUDA_qTMD/tests/full_TMD/data/qTMD/S8T8_aurora.qTMD.0.GI_PDF.D.ex.x7y3z5t7.1HYP_GSRC_W90_k3_T5.PX0PY0PZ9dt4.PpUnpol.h5 /lus/flare/projects/StructNGB/jinchen/package/PyQUDA_qTMD/tests/full_TMD/data/qTMD/S8T8_local.qTMD.0.GI_PDF.D.ex.x7y3z5t7.1HYP_GSRC_W90_k3_T5.PX0PY0PZ9dt4.PpUnpol.h5 > logs/diff_3pt_GI.txt
